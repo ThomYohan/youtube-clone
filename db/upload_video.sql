de0321587382fa627d0b961a1fb143dee69b226e
jrmccann2 <jshmccann@gmail.com>
2018-11-28 10:46:41 -0700

    added in upload to s3

--- db/upload_video.sql
+++ db/upload_video.sql
@@ -1,2 +1,2 @@
 INSERT INTO video (video_url, user_id, category, title, video_desc, thumbnail)
-VALUES ($1, $2, $3, $4, $5, $6)
+VALUES ($1, null, $3, $4, $5, $6)
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import routes from './routes';
+import Upload from './Components/Upload/Upload.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         { routes }
+        <Upload />
       </div>
     );
   }
