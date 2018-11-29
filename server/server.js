24ddee705603ab7e386d1aea27b22c59aa5017e1
Kyle Willcocks <kylewill92@Kyles-MacBook-Pro-2.local>
2018-11-29 13:48:33 -0700

    third times the charm

--- server/server.js
+++ server/server.js
@@ -99,11 +99,8 @@ app.get('/api/signs3', (req, res) => {
     });
 });
 
-<<<<<<< HEAD
 app.get('/api/video-categories', controller.getVidoesByCategory)
 app.get('/api/by-view', controller.getVidoesByViews)
-=======
->>>>>>> master
 
 app.post('/api/upload', controller.upload)
 
