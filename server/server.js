e969d276d29fe5e330eaa64f44aa2dec3b0754e7
MyNameMatters <ellistanner.3@gmail.com>
2018-12-04 15:10:25 -0700

    pulling recent master branch

--- server/server.js
+++ server/server.js
@@ -103,8 +103,7 @@ app.get('/api/signs3', (req, res) => {
 });
 
 
-app.get('/api/userinfo', controller.getUser)
-app.get('/api/video-categories', controller.getVidoesByCategory)
+// app.get('/api/userinfo', controller.getUser)
 
 // organize endpoints by request type
 app.get('/api/video-categories/:category/:id', controller.getVidoesByCategory)
