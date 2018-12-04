907c8a5ab3828c1560f90e99edb7be27d59c59ff
Kyle Willcocks <kylewill92@Kyles-MacBook-Pro-2.local>
2018-12-04 15:33:25 -0700

    fixed recommended for josh

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import axiox from 'axios';
 
 class Search extends Component {
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
 
     render(){
+        //filter
+        //map
         return(
             <div>
 
