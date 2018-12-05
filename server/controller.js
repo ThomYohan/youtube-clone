6042e11f5ce05f674c8d5a0fc119372562c1cdf7
Danielle Tolar <mitchell.habel30@gmail.com>
2018-12-05 09:38:31 -0700

    starting day with updating github

--- server/controller.js
+++ server/controller.js
@@ -137,10 +137,11 @@ module.exports = {
                 console.log(err) 
             })
     },
-    getUser: (req, res) => {res =>
-        res.status(200).send(req.session.user).catch(err => {
+    getUser: (req, res) => { 
+        res.send(req.session.user).catch(err => {
             res.status(500).send({ errorMessage: "Something went wrong" })
             console.log(err)
+            //This comment is to fix github
         })
     }
 }
